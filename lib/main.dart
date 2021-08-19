be63dc0d766ee0cd51b18510b3f74320bb80abf8
SAM <saiyedmustaq786@gmail.com>
2021-08-19 22:57:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                  Colors.deepOrange,
+                  Colors.deepOrangeAccent
+                ])),
+                child: Text("This is drawer header"),
+              ),
+              CustomListStyle(Icons.person, "Menu-1", () {}),
+              CustomListStyle(Icons.person, "Menu-2", () {}),
+              CustomListStyle(Icons.person, "Menu-3", () {}),
+              CustomListStyle(Icons.person, "Menu-4", () {}),
+              CustomListStyle(Icons.person, "Menu-5", () {}),
+            ],
+          ),
+        ));
+  }
+}
+
+class CustomListStyle extends StatelessWidget {
+  IconData icon;
+  String text;
+  Function onTap;
+
+  CustomListStyle(this.icon, this.text, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: Container(
+        decoration:
+            BoxDecoration(border: Border(top: BorderSide(color: Colors.grey))),
+        child: InkWell(
+          splashColor: Colors.orangeAccent,
+          onTap: onTap,
+          child: Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        text,
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(Icons.add)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
